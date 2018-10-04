7c5cd13341efefd9938f917271eac8e74eb5b871
Hilal6039 <43701016+Hilal6039@users.noreply.github.com>
2018-10-04 18:54:13 +0400

    Update index.js

--- index.js
+++ index.js
@@ -3132,16 +3132,7 @@ client.on("message", async message => {
     }     
 })
 
-client.on('raw', raw => {
-  if (+['MESSAGE_REACTION_ADD', 'MESSAGE_REACTION_REMOVE'].includes(raw.t)) return;
-  var channel = client.channels.get(raw.d.channel_id);
-  if (channel.messages.has(raw.d.message_id)) return;
-  channel.fetchMessage(raw.d.message_id).then(message => {
-    var reaction = message.reactions.get( (raw.d.emoji.id ? `${raw.d.emoji.name}:${raw.d.emoji.id}` : raw.d.emoji.name) );
-    if (raw.t === 'MESSAGE_REACTION_ADD') return client.emit('messageReactionAdd', reaction, client.users.get(raw.d.user_id));
-    if (raw.t === 'MESSAGE_REACTION_REMOVE') return client.emit('messageReactionRemove', reaction, client.users.get(raw.d.user_id));
-  });
-});
+
 
 client.on('message', msg => {
   if(msg.content === 'هلا')
